041a88d266a7c676b6bf93dd5504f14ec6e2966c
Lee Junhee <junhee0110@gmail.com>
2021-03-11 20:13:23 +0900

    Update

--- C/KNK_note/CH5/Code/pp5_4.c
+++ C/KNK_note/CH5/Code/pp5_4.c
@@ -7,5 +7,25 @@
 
 int main(void)
 {
-    
+    float wind;
+
+    printf("Enter Wind Speed (in knots) : ");
+    scanf("%f", &wind);
+
+    printf("The corresponding description is ");
+
+    if (wind < 1)
+        printf("Calm\n");
+    else if (wind <= 3)
+        printf("Light air\n");
+    else if (wind <= 27)
+        printf("Breeze\n");
+    else if (wind <= 47)
+        printf("Gale\n");
+    else if (wind <= 63)
+        printf("Storm\n");
+    else
+        printf("Hurricane\n");
+
+    return 0;
 }
